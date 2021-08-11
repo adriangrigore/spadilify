59c4908bebcb8aaa6987e92ef56b8838b4eac3d7
adi <adrian.emil.grigore@gmail.com>
2021-08-11 10:00:33 +0200

    Add mkws.sh

---
+++ _data/sites/mkws.js
@@ -0,0 +1,18 @@
+module.exports = {
+	name: "mkws", // optional, falls back to object key
+	description: "A small, no bloat, minimalist static site generator.",
+	options: {
+		frequency: 60, // (in minutes), 1 hour
+	},
+	urls: [
+		"https://mkws.sh/",
+		"https://mkws.sh/docs.html",
+		"https://mkws.sh/pp.html",
+		"https://mkws.sh/lts.html",
+		"https://mkws.sh/blog.html",
+		"https://mkws.sh/p/Welcome.html",
+		"https://mkws.sh/p/How%20to%20add%20a%20navigation%20menu.html",
+		"https://mkws.sh/p/How%20to%20add%20custom%20titles.html",
+		"https://mkws.sh/p/How%20to%20render%20%60Markdown%60.html",
+	]
+};
