f9c95facbdf83c6d20b2c1a38e69765f397a198a
adi <adrian.emil.grigore@gmail.com>
2021-09-13 02:47:13 +0200

    Remove mkws blog

--- _data/sites/mkws.js
+++ _data/sites/mkws.js
@@ -10,10 +10,5 @@ module.exports = {
 		"https://mkws.sh/docs.html",
 		"https://mkws.sh/pp.html",
 		"https://mkws.sh/lts.html",
-		"https://mkws.sh/blog.html",
-		"https://mkws.sh/p/Welcome.html",
-		"https://mkws.sh/p/How%20to%20add%20a%20navigation%20menu.html",
-		"https://mkws.sh/p/How%20to%20add%20custom%20titles.html",
-		"https://mkws.sh/p/How%20to%20render%20%60Markdown%60.html",
 	]
 };
